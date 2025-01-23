e0c6f2b1f55546aeca5213c62c4f12280fac0167
Michael Moser <49706759+Zesshi@users.noreply.github.com>
2025-01-23 19:57:24 +0100

    Update ssh_brute_force_detector_systemd.py

--- ssh_brute_force_detector_systemd.py
+++ ssh_brute_force_detector_systemd.py
@@ -4,6 +4,42 @@ from cysystemd.reader import JournalReader, JournalOpenMode
 import json
 import sys
 
+def compute_failure_event(session, event, time_window, threshold):
+    event_message = event['MESSAGE']
+    print(event_message)
+
+    if 'Failed password' not in event_message:
+        return
+
+    print('[+] Invalid Login detected')
+
+    src_ip = event_message.split()[5]
+    timestamp_seconds = int(event['_SOURCE_REALTIME_TIMESTAMP']) / 1000000
+
+    if src_ip not in session:
+        #Creating new session entry
+        session[src_ip] = {
+            'timestampSeconds' : timestamp_seconds,
+            'counter' : 1
+        }
+    else:
+        #Increase counter if there was an event within the time window
+        delta = timestamp_seconds - session[src_ip]['timestampSeconds']
+
+        if delta > time_window:
+            #reset counter
+            session[src_ip]['counter'] = 1
+        else:
+            #increase counter by 1
+            session[src_ip]['counter'] += 1
+            
+        #update TimeStamp
+        session[src_ip]['timestampSeconds'] = timestamp_seconds
+
+        #Evaluate alarm
+        if session[src_ip]['counter'] >= threshold:
+            print("[!] Brute-Force attempt detected. With a maximal timewindow of " + str(time_window) + " seconds, an attacker tried at least " + str(threshold) + " times each window to log in with an invalid password.")
+    
 def main():
     #### Argument Parsing ####
     if len(sys.argv) != 3:
@@ -21,13 +57,16 @@ def main():
 
     poll_timeout = 0.5
 
+    session = {}
+    
     while True:
         try:
             # Read and print log entries
             reader.wait(poll_timeout)
-            for record in reader:
-                print(json.dumps(record.data, indent=1, sort_keys=True))
-
+            for event in reader:
+                if event['SYSLOG_IDENTIFIER'] == 'sshd-session':
+                    compute_failure_event(session, event, time_window, threshold)
+                
         except KeyboardInterrupt:
             print("[-] Stopping SSH Brute Force Detector.")
             break
